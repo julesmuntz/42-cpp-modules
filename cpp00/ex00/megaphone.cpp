0a7d61e7d98c8062da412dd47498884c52e5f0fa
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-03-07 16:16:19 +0100

    ex00 fixed

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 int main(int ac, char **av) {
 	if(ac < 2)
-		return cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0;
+		return cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl, 0;
 	for(int n = 1; n < ac; n++)
 	{
 		for(int i = 0; av[n][i]; i++)
 			cout << (char)toupper(av[n][i]);
-		cout << " ";
 	}
-	cout << "\n";
+	cout << endl;
 	return 0;
 }
